675f49e95227e4391cf319a7f3a15596227afd46
Christy Hanson <chrsthnsn@gmail.com>
2023-07-05 15:25:40 -0600

    updated getDb for database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,27 +12,22 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Add logic to a method that accepts some content and adds it to the database
 // put is updating data in the db.
 export const putDb = async (content) => {
   console.log("PUT to database");
-  // pseudocode
   //  open the db - done
   // create the transaction - done
   // create the store - done 
   // create the request - done 
-  // create the result - 
-  // see line 23 in 10-PWA. The name of the database is jate, so this will be entered in the parens.
-  // include the version we want to use, which is 1 
+  // create the result - done
+  // see line 23 in 10-PWA. The name of the database is jate, so this will be entered in the parens. include the version we want to use, which is 1 
   const jate = await openDB('jate', 1);
-
   // readwrite comes from idb
   // transaction
   const transaction = jate.transaction('jate', 'readwrite');
-
   // store - hold objects in memory
   const store = transaction.objectStore('jate');
-
   // request
   //  id and value in this object needed 
   const request = store.put({
@@ -40,7 +35,6 @@ export const putDb = async (content) => {
     id: 1,
     value: content
   });
-
   // result - include in one line. Use asynchronous request here. 
   //  it waits for the db query which was store.put in the request var.
   const result = await request;
@@ -54,9 +48,14 @@ export const putDb = async (content) => {
 // uses a readonly
 export const getDb = async () => {
   console.log("GET to database");
-  // pseudocode
-
-
+  const jate = await openDB('jate', 1);
+  const transaction = jate.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  const request = store.getAll();
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result: GET submitted successfully', result);
+  return result;
 };
 
 initdb();
